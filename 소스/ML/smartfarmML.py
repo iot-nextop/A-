6b230361e33d2af72a5c21ad1a7920a04c6a6a6a
KimMyoungSeop <49678602+KimMyoungSeop@users.noreply.github.com>
2019-11-10 17:55:41 +0900

    머신러닝 코드수정
    머신러닝 코드수정함 예상값출력파트 수정필요

--- 소스/ML/smartfarmML.py
+++ 소스/ML/smartfarmML.py
@@ -1,56 +1,62 @@
 # Lab 4 Multi-variable linear regression
 import tensorflow as tf
 import numpy as np
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
+from firebase import firebase
+import datetime
+
+firebase = firebase.FirebaseApplication('https://smartfarm-1681b.firebaseio.com/', None)
+i=0
+
+f = open("test.txt",'w')
+
+result_t = list(firebase.get('temperature',None).keys())
+for i in range(len(result_t)):
+    print('temperature : ',firebase.get('temperature','%s'%(result_t[i])))
+    t = firebase.get('temperature','%s' % (result_t[i]))
+    f.write(str(t))
+    f.write(',')
+f.write('\n')
+
+result_h = list(firebase.get('humidity',None).keys())
+for i in range(len(result_h)):
+    print('humidity : ',firebase.get('humidity','%s'%(result_h[i])))
+    f.write(str(firebase.get('humidity', '%s' % (result_h[i]))))
+    f.write(',')
+f.write('\n')
+
+result_eh = list(firebase.get('earthhumidity',None).keys())
+for i in range(len(result_eh)):
+    print('earthhumidity : ',firebase.get('earthhumidity','%s'%(result_eh[i])))
+    f.write(str(firebase.get('earthhumidity', '%s' % (result_eh[i]))))
+    f.write(',')
+
+f.close()
 
 
-
-cred = credentials.Certificate('C:/smartfarm-1681b-firebase-adminsdk-2sevr-cbd30f81c0.json')
-firebase_admin.initialize_app(cred,{
-    'databaseURL' : 'https://smartfarm-1681b.firebaseio.com/'
-})
-
-tf.set_random_seed(777)  # for reproducibility
-
-ref = db.reference('humidity/')
-print(ref.get())
+tf.set_random_seed(777)
+xy = np.genfromtxt('test.txt', delimiter=',', dtype=np.float32)
+x_data = xy[0:2,:-1]
+y_data = xy[2:3,:-1]
 
 
-
-
-xy = np.loadtxt('smartfarm-1681b-export.json', delimiter=',', dtype=np.float32)
-x_data = xy[:, 0:-1]
-y_data = xy[:, [-1]]
-
-
-# Make sure the shape and data are OK
 print(x_data, "\nx_data shape:", x_data.shape)
 print(y_data, "\ny_data shape:", y_data.shape)
 
+X = tf.placeholder(tf.float32, shape=[None, None])
+Y = tf.placeholder(tf.float32, shape=[None, None])
 
-# placeholders for a tensor that will be always fed.
-X = tf.placeholder(tf.float32, shape=[None, 2])
-Y = tf.placeholder(tf.float32, shape=[None, 1])
+W = tf.Variable(tf.random_normal([3, 1]), name='weight')
+b = tf.Variable(tf.random_normal([3]), name='bias')
 
-W = tf.Variable(tf.random_normal([2, 1]), name='weight')
-b = tf.Variable(tf.random_normal([1]), name='bias')
 
-# Hypothesis
 hypothesis = tf.matmul(X, W) + b
-
-# Simplified cost/loss function
 cost = tf.reduce_mean(tf.square(hypothesis - Y))
-
-# Minimize
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.0003)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.00001)
 #(learning_rate=1e-4)
 train = optimizer.minimize(cost)
 
-# Launch the graph in a session.
+
 sess = tf.Session()
-# Initializes global variables in the graph.
 sess.run(tf.global_variables_initializer())
 
 
@@ -60,12 +66,5 @@ for step in range(20001):
     if step % 1000 == 0:
         print(step, "Cost:", cost_val, "\nPrediction:\n", hy_val)
 
-
-# Ask my score
-
-predict = sess.run(hypothesis,feed_dict={X:[[50,25]]})
+predict = sess.run(hypothesis,feed_dict={X:[[25,0,0],[40,0,0]]}) #온도값3개, 습도값 3개입력
 print("예상값",predict)
-
-ref = db.reference() #db 위치 지정
-ref.update({'예측값' : int(predict[0])})
-
