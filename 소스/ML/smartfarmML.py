73f886bf770e97c7a737eb9a0b16d6f130b79d6e
KimMyoungSeop <49678602+KimMyoungSeop@users.noreply.github.com>
2019-11-17 15:27:20 +0900

    머신러닝 적용완료
    머신러닝으로 학습된 데이터를 파이어베이스에 넘긴뒤 데이터를 웹에 띄우는 작업까지 끝남

--- 소스/ML/smartfarmML.py
+++ 소스/ML/smartfarmML.py
@@ -9,44 +9,40 @@ i=0
 
 f = open("test.txt",'w')
 
+result_h = list(firebase.get('humidity',None).keys())
 result_t = list(firebase.get('temperature',None).keys())
+result_eh = list(firebase.get('earthhumidity',None).keys())
+
 for i in range(len(result_t)):
     print('temperature : ',firebase.get('temperature','%s'%(result_t[i])))
     t = firebase.get('temperature','%s' % (result_t[i]))
     f.write(str(t))
     f.write(',')
-f.write('\n')
-
-result_h = list(firebase.get('humidity',None).keys())
-for i in range(len(result_h)):
     print('humidity : ',firebase.get('humidity','%s'%(result_h[i])))
     f.write(str(firebase.get('humidity', '%s' % (result_h[i]))))
     f.write(',')
-f.write('\n')
-
-result_eh = list(firebase.get('earthhumidity',None).keys())
-for i in range(len(result_eh)):
     print('earthhumidity : ',firebase.get('earthhumidity','%s'%(result_eh[i])))
     f.write(str(firebase.get('earthhumidity', '%s' % (result_eh[i]))))
     f.write(',')
+    f.write('\n')
 
 f.close()
 
 
 tf.set_random_seed(777)
 xy = np.genfromtxt('test.txt', delimiter=',', dtype=np.float32)
-x_data = xy[0:2,:-1]
-y_data = xy[2:3,:-1]
+x_data = xy[:, 0:-2]
+y_data = xy[:, [-2]]
 
 
 print(x_data, "\nx_data shape:", x_data.shape)
 print(y_data, "\ny_data shape:", y_data.shape)
 
-X = tf.placeholder(tf.float32, shape=[None, None])
-Y = tf.placeholder(tf.float32, shape=[None, None])
+X = tf.placeholder(tf.float32, shape=[None, 2])
+Y = tf.placeholder(tf.float32, shape=[None, 1])
 
-W = tf.Variable(tf.random_normal([3, 1]), name='weight')
-b = tf.Variable(tf.random_normal([3]), name='bias')
+W = tf.Variable(tf.random_normal([2, 1]), name='weight')
+b = tf.Variable(tf.random_normal([1]), name='bias')
 
 
 hypothesis = tf.matmul(X, W) + b
@@ -66,5 +62,8 @@ for step in range(20001):
     if step % 1000 == 0:
         print(step, "Cost:", cost_val, "\nPrediction:\n", hy_val)
 
-predict = sess.run(hypothesis,feed_dict={X:[[25,0,0],[40,0,0]]}) #온도값3개, 습도값 3개입력
-print("예상값",predict)
+predict = sess.run(hypothesis,feed_dict={X:[[25,48]]}) #온도값, 습도값입력
+print("예상값",int(predict[0]))
+
+
+firebase.post('예측값 log',int(predict[0]))
